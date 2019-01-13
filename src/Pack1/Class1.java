e7e0b7e2e10072c10364aa663bf1dedc2a5dcaa5
mohit <mohit@mohit-PC>
2019-01-13 16:22:13 +0530

    c

--- src/Pack1/Class1.java
+++ src/Pack1/Class1.java
@@ -2,11 +2,15 @@ package Pack1;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class Class1 {
@@ -41,7 +45,12 @@ public class Class1 {
 		//dc.setCapability(ChromeOptions.CAPABILITY, options);
 		driver = new RemoteWebDriver(new URL ("http://mohit-PC:4444/wd/hub"),dc);
 		driver.manage().window().maximize();
-		driver.get("https://google.com/");		
+		driver.get("https://google.com/");	
+		WebDriverWait wait = new WebDriverWait(driver,60);
+		WebElement ele = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='tsf']/div[2]/div/div[1]/div/div[2]/input")));
+		ele.sendKeys("Remote Web driver");
+		WebElement ele2 = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='tsf']/div[2]/div/div[3]/center/input[1]")));
+		ele2.click();
 		Thread.sleep(2000);
 		driver.close();
 		
